ef73f9c4780c1a5314f8e7db6a55942704e0da32
LismiaLila <61299072+kholilahzaki@users.noreply.github.com>
2021-04-28 10:02:29 +0700

    Initial commit

---
+++ Data/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cinema.Models.Movie> Movie { get; set; }
+    }
+}
